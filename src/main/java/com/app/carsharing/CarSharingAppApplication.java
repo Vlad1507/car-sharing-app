1db2b673792c7106bd4e4227856b2436d9b44c3c
Vlad Andriichuk <vlad150096@gmail.com>
2025-09-07 16:37:47 +0200

    Master branch empty project with implemented checkstyle into maven.

---
+++ src/main/java/com/app/carsharing/CarSharingAppApplication.java
@@ -0,0 +1,12 @@
+package com.app.carsharing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarSharingAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarSharingAppApplication.class, args);
+    }
+}
---
+++ src/test/java/com/app/carsharing/CarSharingAppApplicationTests.java
@@ -0,0 +1,12 @@
+package com.app.carsharing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarSharingAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
